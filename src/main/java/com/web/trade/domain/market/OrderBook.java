a6aac77d9fbb29e8e5544a86435f71d1c1ee23d5
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-08-02 22:34:00 +0900

    Listで取得する方式に変更

--- src/main/java/com/web/trade/domain/market/OrderBook.java
+++ src/main/java/com/web/trade/domain/market/OrderBook.java
@@ -3,14 +3,12 @@ package com.web.trade.domain.market;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class OrderBook {
 
 	/** 予想約定価格 */
@@ -20,52 +18,13 @@ public class OrderBook {
 	private final BigDecimal referencePrice;
 
 	/** フル板情報(売) */
-	private final Map<BigDecimal, AskQuote> askFull;
+	private final List<AskQuote> askFull;
 
 	/** フル板情報(買) */
-	private final Map<BigDecimal, BidQuote> bidFull;
-
-	/** 成行売気配 */
-	private final AskQuote askMarketQuote;
-
-	/** 成行買気配 */
-	private final BidQuote bidMarketQuote;
+	private final List<BidQuote> bidFull;
 
 	/** 呼値 */
 	private final BigDecimal tick;
-
-	/**
-	 * コンストラクタ
-	 * @param expectedExcutionPrice
-	 * @param referencePrice
-	 * @param askFull
-	 * @param bidFull
-	 */
-	public OrderBook(final BigDecimal expectedExecutionPrice, final BigDecimal referencePrice,
-			final List<AskQuote> askFull, final List<BidQuote> bidFull, final BigDecimal tick) {
-		this.expectedExecutionPrice = expectedExecutionPrice;
-		this.referencePrice = referencePrice;
-		this.askFull = askFull.stream().collect(Collectors.toMap(Quote::getPrice, UnaryOperator.identity()));
-		this.bidFull = bidFull.stream().collect(Collectors.toMap(Quote::getPrice, UnaryOperator.identity()));
-		this.askMarketQuote = getMarketQuote(askFull, AskQuote::new);
-		this.bidMarketQuote = getMarketQuote(bidFull, BidQuote::new);
-		this.tick = tick;
-	}
-
-	/**
-	 * 成行気配値を取得
-	 * @param <T> 型パラメータ
-	 * @param full
-	 * @param constructor
-	 * @return 成行気配値
-	 */
-	private static <T extends Quote> T getMarketQuote(final List<T> full,
-			final BiFunction<BigDecimal, BigDecimal, T> constructor) {
-		return full.stream()
-				.filter(e -> e.getPrice() == null)
-				.findFirst()
-				.orElse(constructor.apply(null, BigDecimal.ZERO));
-	}
 
 	public BigDecimal getBasePrice() {
 		return expectedExecutionPrice != null ? expectedExecutionPrice : referencePrice;
@@ -73,12 +32,12 @@ public class OrderBook {
 
 	public BigDecimal getBaseAskQty() {
 		final BigDecimal basePrice = getBasePrice();
-		return getBetterQty(basePrice, askFull.values());
+		return getBetterQty(basePrice, askFull);
 	}
 
 	public BigDecimal getBaseBidQty() {
 		final BigDecimal basePrice = getBasePrice();
-		return getBetterQty(basePrice, bidFull.values());
+		return getBetterQty(basePrice, bidFull);
 	}
 
 	public BigDecimal getBestAskPrice() {
@@ -103,12 +62,12 @@ public class OrderBook {
 
 	public BigDecimal getBestAskQty() {
 		final BigDecimal bestPrice = getBestAskPrice();
-		return getBetterQty(bestPrice, askFull.values());
+		return getBetterQty(bestPrice, askFull);
 	}
 
 	public BigDecimal getBestBidQty() {
 		final BigDecimal bestPrice = getBestAskPrice();
-		return getBetterQty(bestPrice, bidFull.values());
+		return getBetterQty(bestPrice, bidFull);
 	}
 
 	private static <T extends Quote> BigDecimal getBetterQty(final BigDecimal price, final Collection<T> collection) {
