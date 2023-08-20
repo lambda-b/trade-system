249dfcec6574b5eb37989fdcf54f83b916285673
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-08-20 17:09:31 +0900

    注文ドメインのフィールド作成

--- src/main/java/com/web/trade/domain/order/Order.java
+++ src/main/java/com/web/trade/domain/order/Order.java
@@ -1,8 +1,72 @@
 package com.web.trade.domain.order;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import com.web.trade.enums.HoldingType;
+import com.web.trade.enums.OrdStatus;
+import com.web.trade.enums.OrdType;
+import com.web.trade.enums.SideOfOrder;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * 注文
  */
+@Getter
+@Builder
 public class Order {
 
+	/** 注文番号 */
+	private final String orderId;
+
+	/** 銘柄コード */
+	private final String securityCode;
+
+	/** 預り区分 */
+	private final HoldingType holdingType;
+
+	/** 売買区分 */
+	private final SideOfOrder side;
+
+	/** 注文種別 */
+	private final OrdType orderType;
+
+	/** 注文状態 */
+	private final OrdStatus status;
+
+	/** 注文(指値)価格 */
+	private final BigDecimal limitPrice;
+
+	/** 注文数量 */
+	private final BigDecimal qty;
+
+	/** 注文日時 */
+	private final LocalDateTime receiptDateTime;
+
+	/** 約定数量 */
+	private final BigDecimal execQty;
+
+	/** 失効数量 */
+	private final BigDecimal expiredQty;
+
+	/** 約定リスト */
+	private final List<Execution> executions;
+
+	/** 概算値 */
+	@Getter(AccessLevel.NONE)
+	private final OrderEstimate estimate;
+
+	/**
+	 * 本来概算値が存在しないことは業務的にありえないが、<br>
+	 * APIから取得できないことを加味してOptionalの取扱いにする
+	 * @return 概算値
+	 */
+	public Optional<OrderEstimate> getEstimate() {
+		return Optional.ofNullable(estimate);
+	}
 }
--- src/main/java/com/web/trade/domain/order/OrderEstimate.java
+++ src/main/java/com/web/trade/domain/order/OrderEstimate.java
@@ -1,8 +1,27 @@
 package com.web.trade.domain.order;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * 注文見積もり
  */
+@Getter
+@Builder
 public class OrderEstimate {
 
+	/** 概算約定代金 */
+	private final BigDecimal execAmount;
+
+	/** 概算受渡金額 */
+	private final BigDecimal settlementAmount;
+
+	/** 受渡予定日 */
+	private final LocalDate settlementDate;
+
+	/** 概算手数料 */
+	private final BigDecimal commission;
 }
